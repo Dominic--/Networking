fa02194e228616d4b133d19dd7b558b58f07cada
Dominic-- <linheng_mail@126.com>
2014-11-15 19:43:50 +0800

    complete dijkstra & run off.sh

--- experiment/cplex/deep_first_search_path.py
+++ experiment/cplex/deep_first_search_path.py
@@ -16,6 +16,8 @@ class Graph(object):
         u,v,w = edge
         if(v not in self.node_neighbors[u]):
             self.node_neighbors[u].append([v, w])
+        if(u not in self.node_neighbors[v]):
+            self.node_neighbors[v].append([u, w])
 
     def nodes(self):
         return self.node_neighbors.keys()
@@ -30,6 +32,15 @@ class Graph(object):
                     m = q[i]
                     key = i
             return key
+        
+        path = []
+        def get_path(v, p):
+            if p[v] != None:
+                get_path(p[v], p)
+                path.append(v)
+            else:
+                path.append(v)
+                
 
         path = []
         queue = {}
@@ -37,24 +48,32 @@ class Graph(object):
             queue[n] = 0
 
         queue[s] = 100000
+        parents = {}
+        parents[s] = None
         while True:
             node = find_max(queue)
+            #print("when %d" % node)
             w = queue[node]
-            path.append(node)
 
             if node == v:
+                get_path(v, parents)
                 return path
             else:
+                #print("before : ")
+                #print(queue)
                 for n in self.node_neighbors[node]:
                     if n[0] not in self.visited:
-                        weight = 0
+                        weight = w
                         if w > n[1]:
                             weight = n[1]
-                        else:
-                            weight = w
 
                         if weight > queue[n[0]]:
                             queue[n[0]] = weight
+                            parents[n[0]] = node
+
+                #print("after : ")
+                #print(queue)
+
                 queue.pop(node)
 
             self.visited[node] = True
@@ -98,12 +117,13 @@ class Graph(object):
 if __name__ == '__main__':
 	g = Graph()
 	g.add_nodes([i for i in range(4)])
-	g.add_edge((0, 1, 2))
+	g.add_edge((0, 1, 10))
 	g.add_edge((0, 2, 5))
 	g.add_edge((1, 3, 3))
-	g.add_edge((2, 1, 1))
-	g.add_edge((2, 3, 4))
-	print("nodes:", g.nodes())
+	g.add_edge((2, 1, 20))
+	g.add_edge((2, 3, 6))
+        #print(g.node_neighbors)
+	#print("nodes:", g.nodes())
 
 	#paths = g.find_path(0, 3)
 	path = g.dfs_path(0, 3)
