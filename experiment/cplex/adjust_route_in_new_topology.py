940d8c76570f750cef4d0cb6271f38b569465498
Dominic-- <linheng_mail@126.com>
2014-11-15 16:09:38 +0800

    add dfs path for remove link

--- experiment/cplex/adjust_route_in_new_topology.py
+++ experiment/cplex/adjust_route_in_new_topology.py
@@ -2,8 +2,9 @@ import os
 import paper_cplex_input_without_tm as cplex
 import utilization_from_route as utils
 import parse_xml as xml
+from deep_first_search_path import *
 
-def adjust_route(remove_file, routes):
+def adjust_route(topology, remove_file, routes):
     f = open(remove_file)
     links = []
     line = f.readline()
@@ -15,12 +16,33 @@ def adjust_route(remove_file, routes):
         links.append((s[1],s[0]))
         line = f.readline()
     f.close()
+    
+    g = Graph()
+    f = open(topology)
+    line = f.readline()
+    line = f.readline()
+    while line:
+        s = line.rstrip().split(' ')
+
+        g.add_nodes([int(s[0]), int(s[1])])
+        g.add_edge((int(s[0]), int(s[1]), float(s[2])))
+        g.add_edge((int(s[1]), int(s[0]), float(s[2])))
+
+        line = f.readline()
+    f.close()
+
+    path_replace = dict()
+    for ls,ld in links:
+        path_replace[ls,ld] = g.dfs_path(ls,ld)
+
 
     for ls,ld in links:
         #print(ls,ld)
         for s,d in routes:
             for i in range(len(routes[s,d])):
                 for j in range(len(routes[s,d][i][0]) - 1):
+                    #print("ls, ld : %d %d" % (ls, ld))
+                    #print("len : %d, j : %d" % (len(routes[s,d][i][0]), j))
                     if routes[s,d][i][0][j] == ls and routes[s,d][i][0][j+1] == ld:
                         weight = 0
                         index = 0
@@ -28,6 +50,6 @@ def adjust_route(remove_file, routes):
                             if routes[ls,ld][k][1]> weight:
                                 index = k
                                 weight = routes[ls,ld][k][1]
-                        routes[s,d][i] = [routes[s,d][i][0][0:j] + routes[ls,ld][k][0] + routes[s,d][0][j+2:], routes[s,d][i][1]]
+                        routes[s,d][i] = [routes[s,d][i][0][0:j] + path_replace[ls, ld] + routes[s,d][0][j+2:], routes[s,d][i][1]]
 
     return routes
--- experiment/cplex/deep_first_search_path.py
+++ experiment/cplex/deep_first_search_path.py
@@ -1,63 +1,99 @@
 class Graph(object):
 
-	def __init__(self,*args,**kwargs):
-		self.node_neighbors = {}
-		self.visited = {}
+    def __init__(self,*args,**kwargs):
+        self.node_neighbors = {} 
+        self.visited = {}
 
-	def add_nodes(self,nodelist):
-		for node in nodelist:
-			self.add_node(node)
+    def add_nodes(self,nodelist): 
+        for node in nodelist: 
+            self.add_node(node)
+    
+    def add_node(self,node): 
+        if not node in self.nodes(): 
+            self.node_neighbors[node] = [] 
 
-	def add_node(self,node):
-		if not node in self.nodes():
-			self.node_neighbors[node] = []
+    def add_edge(self,edge):
+        u,v,w = edge
+        if(v not in self.node_neighbors[u]):
+            self.node_neighbors[u].append([v, w])
 
-	# u for source
-	# v for destination
-	# w for weight
-	def add_edge(self,edge):
-		u,v,w = edge
-		if(v not in self.node_neighbors[u]):
-			self.node_neighbors[u].append([v, w])
+    def nodes(self):
+        return self.node_neighbors.keys()
 
-	def nodes(self):
-		return self.node_neighbors.keys()
+    def dfs_path(self, s, v):
+        self.visited = {}
+        def find_max(q):
+            m = 0
+            key = 0
+            for i in q:
+                if q[i] > m:
+                    m = q[i]
+                    key = i
+            return key
+
+        path = []
+        queue = {}
+        for n in self.nodes():
+            queue[n] = 0
+
+        queue[s] = 100000
+        while True:
+            node = find_max(queue)
+            w = queue[node]
+            path.append(node)
+
+            if node == v:
+                return path
+            else:
+                for n in self.node_neighbors[node]:
+                    if n[0] not in self.visited:
+                        weight = 0
+                        if w > n[1]:
+                            weight = n[1]
+                        else:
+                            weight = w
+
+                        if weight > queue[n[0]]:
+                            queue[n[0]] = weight
+                queue.pop(node)
+
+            self.visited[node] = True
 		
-	def find_path(self, s, v):
-		paths = []
-		path = []
-		def dfs(node):
-			self.visited[node] = True
-			path.append(node)
+    def find_path(self, s, v):
+        paths = []
+        path = []
+        def dfs(node):
+            self.visited[node] = True
+            path.append(node)
 
-			if node == v:
-				min_weight = 10000
-				for i in range(len(path) - 1):
-					for n in self.node_neighbors[path[i]]:
-						if n[0] == path[i+1] and min_weight > n[1]:
-							min_weight = n[1]
-				
-				for i in range(len(path) - 1):
-					for n in self.node_neighbors[path[i]]:
-						if n[0] == path[i+1]:
-							n[1] -= min_weight
-				
-				paths.append([[i for i in path], min_weight])
-				path.remove(node)
-				
-				self.visited.pop(node, None)
-				return
+            if node == v:
+                min_weight = 10000
+                for i in range(len(path) - 1):
+                    for n in self.node_neighbors[path[i]]:
+                        if n[0] == path[i+1] and min_weight > n[1]:
+                            min_weight = n[1]
+                
+                for i in range(len(path) - 1):
+                    for n in self.node_neighbors[path[i]]:
+                        if n[0] == path[i+1]:
+                            n[1] -= min_weight
+                
+                paths.append([[i for i in path], min_weight])
+                path.remove(node)
+                
+                self.visited.pop(node, None)
+                return
 
-			for n in self.node_neighbors[node]:
-				if not n[0] in self.visited and n[1] > 0:
-					dfs(n[0])
-			
-			self.visited.pop(node, None)
-			path.remove(node)
-			
-		dfs(s)
+            for n in self.node_neighbors[node]:
+                if not n[0] in self.visited and n[1] > 0:
+                    dfs(n[0])
 
-		return paths
+            self.visited.pop(node, None)
+            path.remove(node)
+                        
+        dfs(s)
+
+        return paths
 
 if __name__ == '__main__':
 	g = Graph()
@@ -69,5 +105,6 @@ if __name__ == '__main__':
 	g.add_edge((2, 3, 4))
 	print("nodes:", g.nodes())
 
-	paths = g.find_path(0, 3)
-	print(paths)
+	#paths = g.find_path(0, 3)
+	path = g.dfs_path(0, 3)
+	print(path)
